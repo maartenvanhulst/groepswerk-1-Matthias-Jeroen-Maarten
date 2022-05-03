4d89ecff8fd93967e924d2c96ce49436918e6993
Maarten Vanhulst <maarten_vanhulst@hotmail.com>
2022-05-03 20:01:18 +0200

    Modified database.py to work with postgres

--- src/database.py
+++ src/database.py
@@ -1,23 +1,29 @@
-import mysql.connector
+import psycopg2
 import yaml
+import os
 
-from errors import *
+from src.errors import *
 
 __CONNECTION = None
 
+
 def db_get_connection():
     global __CONNECTION
+
+    while os.getcwd()[-12:] != 'groepswerk-1':
+        os.chdir('..')
 
     try:
         if __CONNECTION is None:
             with open("connection.secret", "r") as connect_options:
                 options = yaml.load(connect_options, Loader=yaml.FullLoader)
 
-            __CONNECTION = mysql.connector.connect(**options)
+            __CONNECTION = psycopg2.connect(
+                f"dbname={options['database']} user={options['user']} password={options['password']}")
 
         return __CONNECTION
 
-    except mysql.connector.Error as e:
+    except psycopg2.OperationalError as e:
         handle_fatal(FATAL_CANNOT_CREATE_CONNECTION, e)
 
     except Exception as e:
@@ -26,11 +32,11 @@ def db_get_connection():
 
 def db_get_cursor(connection):
     try:
-        cursor = connection.cursor(prepared=True)
+        cursor = connection.cursor()
 
         return cursor
 
-    except mysql.connector.Error as e:
+    except psycopg2.OperationalError as e:
         handle_fatal(FATAL_CANNOT_CREATE_CURSOR, e)
 
     except Exception as e:
@@ -47,64 +53,64 @@ def db_close_connection():
         try:
             connection.close()
 
-        except mysql.connector.Error as e:
+        except psycopg2.OperationalError as e:
             handle_error(ERROR_CANNOT_CLOSE_CONNECTION, e)
 
         except Exception as e:
             handle_unexpected(e)
 
 
-def db_get_records(query):
-    data = None
-    connection = db_get_connection()
-    cursor = db_get_cursor(connection)
-
-    try:
-        cursor.execute(query)
-
-    except mysql.connector.Error as e:
-        handle_fatal(FATAL_ERROR_IN_QUERY, e)
-
-    except Exception as e:
-        handle_unexpected(e)
-
-    try:
-        data = cursor.fetchall()
-
-    except mysql.connector.Error as e:
-        handle_fatal(FATAL_ERROR_IN_FETCH_ALL, e)
-
-    except Exception as e:
-        handle_unexpected(e)
-
-    try:
-        cursor.close()
-
-    except mysql.connector.Error as e:
-        handle_error(ERROR_CANNOT_CLOSE_CURSOR, e)
-
-    except Exception as e:
-        handle_unexpected(e)
-
-    return data
-
-
-def db_get_record(query):
-    data = db_get_records(query)
-
-    if data is None:
-        return None
-
-    return data[0]
-
-
-def db_get_value(query):
-    data = db_get_record(query)
-
-    if data is None:
-        return None
-
-    return data[0]
+# def db_get_records(query):
+#     data = None
+#     connection = db_get_connection()
+#     cursor = db_get_cursor(connection)
+#
+#     try:
+#         cursor.execute(query)
+#
+#     except mysql.connector.Error as e:
+#         handle_fatal(FATAL_ERROR_IN_QUERY, e)
+#
+#     except Exception as e:
+#         handle_unexpected(e)
+#
+#     try:
+#         data = cursor.fetchall()
+#
+#     except mysql.connector.Error as e:
+#         handle_fatal(FATAL_ERROR_IN_FETCH_ALL, e)
+#
+#     except Exception as e:
+#         handle_unexpected(e)
+#
+#     try:
+#         cursor.close()
+#
+#     except mysql.connector.Error as e:
+#         handle_error(ERROR_CANNOT_CLOSE_CURSOR, e)
+#
+#     except Exception as e:
+#         handle_unexpected(e)
+#
+#     return data
+#
+#
+# def db_get_record(query):
+#     data = db_get_records(query)
+#
+#     if data is None:
+#         return None
+#
+#     return data[0]
+#
+#
+# def db_get_value(query):
+#     data = db_get_record(query)
+#
+#     if data is None:
+#         return None
+#
+#     return data[0]
 
 
 def db_execute(query, data=None):
@@ -117,26 +123,34 @@ def db_execute(query, data=None):
         else:
             cursor.execute(query, data)
 
-    except mysql.connector.Error as e:
+    except psycopg2.OperationalError as e:
         handle_error(FATAL_ERROR_IN_QUERY, e)
+        # ToDo: add Rollback?
 
     except Exception as e:
         handle_unexpected(e)
+        # ToDo: add Rollback?
 
     try:
         connection.commit()
 
-    except mysql.connector.Error as e:
-        handle_error(FATAL_ERROR_IN_COMMIT, e)
+    # except mysql.connector.Error as e:
+    #     handle_error(FATAL_ERROR_IN_COMMIT, e)
 
     except Exception as e:
         handle_unexpected(e)
+        # ToDo: add Rollback?
 
     try:
         cursor.close()
 
-    except mysql.connector.Error as e:
-        handle_error(ERROR_CANNOT_CLOSE_CURSOR, e)
+    # except mysql.connector.Error as e:
+    #     handle_error(ERROR_CANNOT_CLOSE_CURSOR, e)
 
     except Exception as e:
         handle_unexpected(e)
+
+# while os.getcwd()[-12:] != 'groepswerk-1':
+#     os.chdir('..')
+#
+# db_execute(open('./src/database/create_db.sql', 'r').read())
