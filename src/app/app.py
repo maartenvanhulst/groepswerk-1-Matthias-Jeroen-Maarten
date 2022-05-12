7a5946ff2edd274f7b76e803d5b91c00fa47276d
Maarten Vanhulst <maarten_vanhulst@hotmail.com>
2022-05-12 21:42:06 +0200

    Fixed add_clubs.sql for postgres: insert into ... values ...

--- src/app/app.py
+++ src/app/app.py
@@ -5,12 +5,13 @@ import pathlib
 
 app = Flask(__name__, static_url_path="/static", static_folder="templates/static")
 
+list1 = [3, 'xyz?!', 127, 3320, 'Hoegaarden', 'Belgium', '', '', '', '', '', '', '', '', '', True]
 
 @app.route("/")
 def index_html():
     root_dir = os.getcwd()
     d_object = DataObject()
-    query = d_object.db_execute(open(os.path.join(root_dir, 'src', 'database', 'get_clubs.sql'), 'r').read())
+    query = d_object.db_execute(open(os.path.join(root_dir, 'src', 'database', 'add_club.sql'), 'r').read(),data=list1)
     print(query)
     return render_template("index.html", data=None)
 
