61d1c182dcc2b82fe290c9729afe541bad5323ed
Maarten Vanhulst <maarten_vanhulst@hotmail.com>
2022-05-31 21:31:40 +0200

    Bugfix: add_team.sql was empty & team_id in matches not in teams

---
+++ src/database/add_team.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.team(
+	id, name, display_name, home_color_1, home_color_2, home_color_3, away_color_1, away_color_2, away_color_3, alt_color_1, alt_color_2, alt_color_3, club_id)
+	VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
